c60ad9e6b3e3f0a6ec49758aadcdf14db39a7bea
Olga Matoula <olgamatoula@hotmail.com>
2016-01-14 12:39:05 +0200

    Create Day13.py

---
+++ 30-Days-of-Code/Day13.py
@@ -0,0 +1,31 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Book:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def display(self): pass
+
+
+class MyBook(Book):
+
+    def __init__(self, title, author, price):
+        self.price = price
+        super(MyBook, self).__init__(title, author)
+
+    def display(self):
+        print "Title:", self.title
+        print "Author:", self.author
+        print "Price:", self.price
+
+
+title = raw_input()
+author = raw_input()
+price = int(raw_input())
+new_novel = MyBook(title, author, price)
+new_novel.display()
