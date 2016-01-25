8d5578b083b4fb5c74234948be617b653de8f9d3
Olga Matoula <olgamatoula@hotmail.com>
2016-01-25 17:15:55 +0200

    Update Day16.py

--- 30-Days-of-Code/Day16.py
+++ 30-Days-of-Code/Day16.py
@@ -1,21 +1,19 @@
 size = input()
 array = map(int, raw_input().strip().split(' '))
-
+array.sort()
 numbers = []
 min = abs(array[0] - array[1])
 numbers.append(array[0])
 numbers.append(array[1])
 for i in range(size-1):
-    for j in range(i +1, size):
-        if abs(array[i] - array[j]) < min:
-            min = abs(array[i] - array[j])
-            numbers = []
-            numbers.append(array[i])
-            numbers.append(array[j])
-        elif abs(array[i] - array[j]) == min:
-            min = abs(array[i] - array[j])
-            numbers.append(array[i])
-            numbers.append(array[j])
-numbers.sort()
+    if abs(array[i] - array[i+1]) < min:
+        min = abs(array[i] - array[i+1])
+        numbers = []
+        numbers.append(array[i])
+        numbers.append(array[i+1])
+    elif abs(array[i] - array[i+1]) == min:
+        min = abs(array[i] - array[i+1])
+        numbers.append(array[i])
+        numbers.append(array[i+1])
 for n in numbers:
     print n,
