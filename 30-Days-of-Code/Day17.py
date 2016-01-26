61df292f03ce15606b4c076667a64241d1233a49
Olga Matoula <olgamatoula@hotmail.com>
2016-01-26 17:24:34 +0200

    Create Day17.py

---
+++ 30-Days-of-Code/Day17.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def power(self, n, p):
+        if (n < 0) or (p < 0):
+            raise Exception("n and p should be non-negative")
+        result = n ** p
+        return result
+
+
+myCalculator = Calculator()
+T = int(raw_input())
+for i in range(T):
+    n, p = map(int, raw_input().split())
+    try:
+        ans = myCalculator.power(n,p)
+        print ans
+    except Exception, e:
+        print e
